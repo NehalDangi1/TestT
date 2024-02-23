24952aca67f60198aa55e47ce6f3970a18247457
NehalDangi1 <nehal@brainnit.co.uk>
2024-02-24 00:05:11 +0530

    as

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponentComponent } from '../app/test-component/test-component.component'; // Import your test component
 
 const routes: Routes = [
-
-  {path:'', pathMatch: 'full'},
+  // Define your routes here
+  { path: 'testt', component: TestComponentComponent }, // Route for 'testt' segment
+  // Add other routes as needed
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home if no route matches
+  { path: '**', redirectTo: '/not-found' } // Redirect to not-found if the route doesn't match any defined route
 ];
 
 @NgModule({
