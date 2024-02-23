0b3c780ae5268d7f308932f99ab5cbea97223413
NehalDangi1 <nehal@brainnit.co.uk>
2024-02-24 00:11:50 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { TestComponentComponent } from '../app/test-component/test-component.com
 
 const routes: Routes = [
   // Define your routes here
-  { path: 'testt', component: TestComponentComponent }, // Route for 'testt' segment
+  // { path: 'testt', component: TestComponentComponent }, // Route for 'testt' segment
   // Add other routes as needed
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home if no route matches
+  { path: '', redirectTo: '/', pathMatch: 'full' }, // Redirect to home if no route matches
   { path: '**', redirectTo: '/not-found' } // Redirect to not-found if the route doesn't match any defined route
 ];
 
